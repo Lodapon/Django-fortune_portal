709ff9ad089222ab11b5a6d39972020e39d2f1a3
Noppadol <nop_the_last_wizard@hotmail.com>
2025-06-09 21:01:18 +0700

    Add TarotCard model and initial migration

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class TarotCard(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=200)  # Or ImageField if you use media
+    meaning = models.TextField()
+    position = models.CharField(max_length=100, blank=True, null=True)
+    position_description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name

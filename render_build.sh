3a01ddb900b330f5e6483ef345e19f8ca0a5a5d3
Noppadol <nop_the_last_wizard@hotmail.com>
2025-06-09 23:17:57 +0700

    Fix Render config and ensure DB name matches

--- render_build.sh
+++ render_build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+set -o errexit
+
 # Install dependencies
 pip install -r requirements.txt
 
-# Run migrations
+# Run database migrations
+python manage.py makemigrations
 python manage.py migrate
 
 # Collect static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
